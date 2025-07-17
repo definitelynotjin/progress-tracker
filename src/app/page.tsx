2285e6eaa4be49fa47b645e52e6882d31a8f2a96
definitelynotjin <118191197+definitelynotjin@users.noreply.github.com>
2025-07-17 16:45:05 +0700

    holy fuck, i can code now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-
-
 import KanbanBoard from "./components/kanban/KanbanBoard";
 
 export default function Page() {
   return (
-    <div className="bg-red-100 bg-repeat-round min-h-screen p-6">
-      <h1 className="text-3xl font-bold mb-6 text-center text-red-500">Progress Tracker</h1>
+    <div className="bg-gray-500 bg-repeat-round min-h-screen p-6">
+      <h1 className="text-3xl font-bold mb-6 text-center ">Progress Tracker</h1>
       <KanbanBoard />
     </div>
   );
